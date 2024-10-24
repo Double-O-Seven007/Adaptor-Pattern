a70c48fe0b1672ed34d007f73c445d146d6d2ce6
007 <84271473+Double-O-Seven007@users.noreply.github.com>
2024-10-24 09:02:34 +0200

    first commit

---
+++ adaptor_pattern/IDuck.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adaptor_pattern
+{
+    public interface IDuck
+    {
+        
+    }
+}
